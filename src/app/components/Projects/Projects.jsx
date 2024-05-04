29dd655be13236f39d52bfd8ee3bfcf0c35f7523
Augusto <eric72001@hotmail.com>
2024-05-04 20:14:03 -0300

    Style: changing margin top project section xs breakpoint

--- src/app/components/Projects/Projects.jsx
+++ src/app/components/Projects/Projects.jsx
@@ -7,7 +7,7 @@ export default function Projects() {
     {name: 'ericaugusto-os', link: 'https://ericaugusto-os.pages.dev', img: '/images/ericaugusto-os.png', description: 'linux rice inspired portfolio website', skills: ['react', 'ts', 'scss', 'figma']},
     {name: 'Discord Clone',img: '/images/discord_clone.png', description: 'Just a discord clone :)', skills: ['next', 'tailwind', 'ts']}];
     console.log(projects)
-    return <section className="my-[180px] lg:mt-24">
+    return <section className="my-[180px] lg:mt-24 xs:mt-10">
         <h2 className="text-5xl lg:text-3xl font-bold mb-3 lg:mb-1">{'// Projects'}</h2>
         <label className='bg-comment-grey h-min flex-grow-0 lg:text-sm'>{`Take a look on what i've being cooking`}</label>
         <ul className='grid grid-cols-3 gap-[60px] my-[30px] dots lg:grid-cols-1'>
