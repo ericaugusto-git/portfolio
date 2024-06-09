518e7221dc81db7322c77ab2b66c3bd38289b762
Augusto <eric72001@hotmail.com>
2024-06-09 15:57:56 -0300

    fix: build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ type Props = {
   children: ReactNode;
 };
 
+export const runtime = 'edge';
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({children}: Props) {
